c75891baecea2e32df6386531b0482c3156a82f4
DivyaSabharwal2023 <158770300+DivyaSabharwal2023@users.noreply.github.com>
2024-02-13 05:09:29 +0000

    conditions

---
+++ 01_basics/Operations.js
@@ -0,0 +1,15 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+
+console.log(2+2)
+console.log("1"+2)
+console.log(2+"1")
+console.log("2"+"2")
+console.log(1+"2"+"2")
+console.log("2"+2+1)
+console.log(2+2+"1")
+
+
+
+
---
+++ 01_basics/conditions.js
@@ -0,0 +1,11 @@
+if(undefined){
+    console.log("if")
+}
+else{
+    console.log("else")
+}
+// if has 1 it will go to if
+// if has 0 it will go to else
+//if has -1 it will go to if still; any number with negative sign considers a number and not null/0
+//if has null it will go to else
+// if has undefined it will still go to else

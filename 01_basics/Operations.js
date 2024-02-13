f600ebb7ba364021802e7130256398bfe2705f60
DivyaSabharwal2023 <158770300+DivyaSabharwal2023@users.noreply.github.com>
2024-02-13 05:44:00 +0000

    operations

--- 01_basics/Operations.js
+++ 01_basics/Operations.js
@@ -7,8 +7,17 @@ console.log("1"+2)
 console.log(2+"1")
 console.log("2"+"2")
 console.log(1+"2"+"2")
-console.log("2"+2+1)
-console.log(2+2+"1")
+console.log("2"+2+1)// coz it starts with string so concatenation has been done
+console.log(2+2+"1")// it starts with number so first got added and then concatenated.
+console.log(+true)//this will give 1
+//console.log(true+)//this will throw error
+console.log(+"")//this gives value 1
+console.log(+false)//this will give 0
+
+
+
+
+
 
 
 
--- 01_basics/conditions.js
+++ 01_basics/conditions.js
@@ -1,4 +1,4 @@
-if(undefined){
+if(""){
     console.log("if")
 }
 else{
@@ -9,3 +9,4 @@ else{
 //if has -1 it will go to if still; any number with negative sign considers a number and not null/0
 //if has null it will go to else
 // if has undefined it will still go to else
+//"" is considered as false hence it goes to else statement
